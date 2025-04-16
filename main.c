93cfad0331133a1f29cd1ccdefa318ed9a32d01d
gift <nziokagift@gmail.com>
2025-04-16 18:39:46 +0300

    Added full code for vehicle behavior

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "vehicle.h"
+
+int main() {
+    int light = 0;
+	int choice;
+
+	srand(time(NULL));
+
+	printf("=== Vehicle Simulation ===\n");
+
+	while (1){
+        printf("\n--- Menu ---\n")
+        printf("1. Add Vehicle\n")
+        printf("2. Change Light (Red <-> Green)\n")
+        printf("3. Update Vehicles (Try to move)\n")
+        printf("4. Show Vehicle Queue\n")
+        printf("0. Exit\n")
+        printf("Enter choice: ")
+        scanf("%d", &choice);
+
+        switch
+    }
+	
+	}
